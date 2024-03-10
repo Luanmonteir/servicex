b8b38914baff39e5ed3dba6f8255dd8aee8e1c81
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2024-03-10 11:38:30 -0300

    Initial commit

---
+++ servicex/src/test/java/br/com/luan/servicex/ServicexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luan.servicex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
