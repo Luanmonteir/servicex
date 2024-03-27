7458a351d52b8f6ac3ffee3bf97f240469a08a7e
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2024-03-27 15:18:04 -0300

    Atualização do código

--- servicex/src/main/java/br/com/luan/servicex/pagamento/resources/PagamentoResource.java
+++ servicex/src/main/java/br/com/luan/servicex/pagamento/resources/PagamentoResource.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pagametos")
-public class PagamentoResources {
+public class PagamentoResource {
     @Autowired
     private PagamentoService pagamentoService;
 
